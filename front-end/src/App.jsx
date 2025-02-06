95e8753d6167a27f990114650c7824df1433565c
Arno Pan <apan11@my.bcit.ca>
2025-02-05 18:26:59 -0800

    changed schema of arry of goals object to include date as integer for the new second property date, added addGoal function input button works

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -5,12 +5,36 @@ function App() {
 
   // Array of Goals Objects using useState to update and track
   const [goals, setGoals] = useState([
-    { id: 1, text: "Goal 1", completed: false},
-    { id: 2, text: "Goal 2", completed: false},
-    { id: 3, text: "Goal 3", completed: false},
-    { id: 4, text: "Goal 4", completed: false},
-    { id: 5, text: "Goal 5", completed: false}
+    { id: 1, date: 1696886400000, text: "Goal 1", completed: false},
+    { id: 2, date: 1696800000000, text: "Goal 2", completed: false},
+    { id: 3, date: 1696713600000, text: "Goal 3", completed: false},
+    { id: 4, date: 1696627200000, text: "Goal 4", completed: false},
+    { id: 5, date: 1696886400000, text: "Goal 5", completed: false}
   ]);
+
+  // Keep track of new Goal to be added (using input in Input Section)
+  const [newGoal, setNewGoal] = useState("");
+
+    // Handle Adding Goals
+    const addGoal = () => {
+      if(newGoal === "") {
+        alert("Goal has not been entered.")
+        console.log(`Goal: \"${newGoal}\" is empty.`);
+        return;
+      }
+
+      console.log(`Goal: \"${newGoal}\" has been entered in input.`);
+
+      setGoals(
+        [...goals, {
+          id: ++goals.length,
+          date: Date.now(),
+          text: newGoal,
+          completed: false
+        }]
+      );
+
+    }
 
   return (
     <div className="app-container">
@@ -18,8 +42,8 @@ function App() {
 
         {/* Input Section */}
         <div className="input-section">
-          <input type="text" placeholder="Enter your focus goal."/>
-          <button>Goal Button</button>
+          <input type="text" placeholder="Enter your focus goal." value={newGoal} onChange={(e) => setNewGoal(e.target.value)}/>
+          <button onClick={addGoal}>Add Goal</button>
         </div>
 
         {/* Goal List */}
@@ -29,7 +53,7 @@ function App() {
             {
               goals.slice(0,3).map((goal) => (
                 <li key={goal.id}>
-                  Top #{goal.id}: "{goal.text}" <button>Complete</button> - <button>Edit</button> - <button>Delete</button>
+                  Top #{goal.id} - Date: {Date(goal.date)} "{goal.text}" <button>Complete</button> - <button>Edit</button> - <button>Delete</button>
                 </li>
               ))
             }
